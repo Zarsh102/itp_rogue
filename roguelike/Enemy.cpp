9eaa3dde832d36ce379f56e177884e712eb3f134
Chris Janes <gingoreilly@gmail.com>
2025-01-21 15:52:37 +0000

    update Enemy with MoveTowardsPlayer function

--- roguelike/Enemy.cpp
+++ roguelike/Enemy.cpp
@@ -1,1 +1,50 @@
 #include "Enemy.h"
+
+#include "Grid.h"
+
+const char Enemy::enemySymbol = '!';
+
+void Enemy::MoveTowardsPlayer(int playerX, int playerY, 
+	const std::vector< std::vector<char>>& grid, int colSize, int rowSize)
+{
+	// move right towards player
+	if (x < playerX)
+	{
+		if ((x < rowSize - 1) &&
+			(grid[x + 1][y] != Grid::wallSymbol))
+		{
+			x++;
+			return;
+		}
+	}
+
+	if (x > playerX) 
+	{	
+		if ((x > 1) &&
+			(grid[x - 1][y] != Grid::wallSymbol))
+		{
+			x--;
+			return;
+		}
+	}
+
+	if (y < playerY)
+	{
+		if ((y < colSize - 1) &&
+			(grid[x][y + 1] != Grid::wallSymbol))
+		{
+			y++;
+			return;
+		}
+	}
+
+	if (y > playerY)
+	{
+		if ((y > 1) &&
+			(grid[x][y - 1] != Grid::wallSymbol))
+		{
+			y--;
+			return;
+		}
+	}
+}
--- roguelike/Enemy.h
+++ roguelike/Enemy.h
@@ -1,8 +1,29 @@
 #pragma once
 
+#include <vector>
+
 class Enemy {
 public:
+	Enemy() {}
+	Enemy(int xPos, int yPos) : x{ xPos }, y{ yPos } { }
+	Enemy(int xPos, int yPos, int hp) : x{ xPos }, y{ yPos }, health{ hp } {}
+
+	void MoveTowardsPlayer(int playerX, int playerY, 
+		const std::vector< std::vector<char>>& grid, 
+		int colSize, int rowSize);
+	
+	int getX() { return x; }
+	int getY() { return y; }
+
+	void setPosition(int xPos, int yPos) {
+		x = xPos;
+		y = yPos;
+	}
+
+	static const char enemySymbol;
 
 private:
-	const char enemySymbol = '!';
+	int x{ 0 }, y{ 0 };
+	int health{ 10 };
+	
 };
